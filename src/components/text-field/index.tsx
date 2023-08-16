b57dcc8de19cd7f132f18f0c57354f77896a9a80
mangming2 <leejiho35@gmail.com>
2023-08-16 16:32:05 +0900

    Update : add box shadow at gnb

--- src/components/text-field/index.tsx
+++ src/components/text-field/index.tsx
@@ -1,4 +1,3 @@
-import styled from '@emotion/styled';
 import { InputHTMLAttributes } from 'react';
 import tw from 'twin.macro';
 
@@ -8,14 +7,13 @@ import { formatNumberWithCommas } from '~/utils/string';
 import { IconDown } from '../icons';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  value?: string;
   amount?: string;
-  label?: string;
+
   error?: boolean;
   errorMessage?: string;
 }
 
-export const TextField = ({ value, amount, label, error, errorMessage, ...rest }: Props) => {
+export const TextField = ({ amount, ...rest }: Props) => {
   return (
     <Wrapper>
       <InputWrapper>
@@ -35,9 +33,7 @@ export const TextField = ({ value, amount, label, error, errorMessage, ...rest }
       {amount && (
         <CaptionWrapper>
           <CaptionText>Balance</CaptionText>
-          <CaptionBalance error={error}>
-            {error ? errorMessage : ` ${formatNumberWithCommas(amount)} `}
-          </CaptionBalance>
+          <CaptionBalance>{`${formatNumberWithCommas(amount)}`}</CaptionBalance>
         </CaptionWrapper>
       )}
     </Wrapper>
@@ -49,7 +45,7 @@ const Wrapper = tw.div`
 `;
 
 const InputWrapper = tw.div`
-    w-full flex items-center gap-8
+  w-full flex items-center gap-8
   `;
 
 const Input = tw.input`
@@ -59,33 +55,28 @@ const Input = tw.input`
 `;
 
 const DropdownWrapper = tw.div`
-    flex items-center gap-8 py-8 px-12 bg-gray1 rounded-24
+  flex items-center gap-8 py-8 px-12 bg-gray1 rounded-24
 `;
 
 const CurrencyImg = tw.img`
-    w-28 h-28
+  w-28 h-28
 `;
 
 const CurrencyName = tw.div`
-    font-m-22 text-gray4
+  font-m-22 text-gray4
 `;
 
 const IconWrapper = tw.div`
-    w-24 h-24 flex-center 
+  w-24 h-24 flex-center 
 `;
 
 const CaptionWrapper = tw.div`
   flex justify-end gap-4
 `;
 
-interface CaptionTextProps {
-  error: boolean | undefined;
-}
-
 const CaptionText = tw.div`
   font-r-14 text-gray3`;
 
-const CaptionBalance = styled.div<CaptionTextProps>(({ error }) => [
-  tw` font-r-14 text-gray4`,
-  error && tw`text-blue`,
-]);
+const CaptionBalance = tw.div`
+  font-r-14 text-gray4
+  `;
