435ef7dc2dcc88a65a75a757e920b2e539ca8301
mangming2 <leejiho35@gmail.com>
2023-08-16 16:20:20 +0900

    Delete : unused svg

--- src/components/card/card-tertiary.tsx
+++ src/components/card/card-tertiary.tsx
@@ -24,24 +24,24 @@ export const CardTertiary = ({ icon, title, content, cardType, ...rest }: Props)
 };
 
 const Wrapper = tw.div`
-    flex flex-col gap-20 w-full
-    py-20 px-24 rounded-20
-    bg-gray1
+  flex flex-col gap-20 w-full
+  py-20 px-24 rounded-20
+  bg-gray1
   `;
 
 const TitleWrapper = tw.div`
-    flex items-center gap-8
+  flex items-center gap-8
 `;
 
 const IconWrapper = tw.div`
-   flex-center w-20 h-20 rounded-4 p-2 gap-10
-   bg-blue
+  flex-center w-20 h-20 rounded-4 p-2 gap-10
+  bg-blue
 `;
 
 const Title = tw.div`
-    font-b-14 text-gray4
+  font-b-14 text-gray4
 `;
 
 const ContentText = tw.div`
-    font-m-24 text-black
+  font-m-24 text-black
 `;
