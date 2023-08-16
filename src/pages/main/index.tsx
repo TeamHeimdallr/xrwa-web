9d1b8dcca0181372b9e8420954f980dac51f6522
mangming2 <leejiho35@gmail.com>
2023-08-16 18:26:47 +0900

    update : landing page

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import tw from 'twin.macro';
 
+import { CardDeal } from '~/components/card/card-deal';
 import { CardPrimary } from '~/components/card/card-primary';
 import { IconActive, IconLocked, IconTotal } from '~/components/icons';
 
@@ -25,6 +26,25 @@ const MainPage = () => {
           <CardPrimary icon={<IconActive />} title="Number of Active Deals" content="1" />
         </DashBoardCardWrapper>
       </DashBoardWrapper>
+      <DealsWrapper>
+        <DealsTitle>Deals</DealsTitle>
+        <DealsCardWrapper>
+          <CardDeal
+            image="https://via.placeholder.com/150"
+            title="Deal Title"
+            contents="Deal Contents"
+            value="1234567"
+            apy="5.3"
+          />
+          <CardDeal
+            image="https://via.placeholder.com/150"
+            title="Deal Title"
+            contents="Deal Contents"
+            value="1234567"
+            apy="5.3"
+          />
+        </DealsCardWrapper>
+      </DealsWrapper>
     </Wrapper>
   );
 };
@@ -41,6 +61,18 @@ const DashBoardTitle = tw.div`
 `;
 
 const DashBoardCardWrapper = tw.div`
+  flex gap-24
+`;
+
+const DealsWrapper = tw.div`
+  flex flex-col w-960 gap-16
+`;
+
+const DealsTitle = tw.div`
+  font-b-24 text-black
+`;
+
+const DealsCardWrapper = tw.div`
   flex gap-24
 `;
 
