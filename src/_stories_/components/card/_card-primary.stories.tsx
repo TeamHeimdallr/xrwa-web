7271206b356b15031ff0eda52fdcf1725428a861
mangming2 <leejiho35@gmail.com>
2023-08-16 18:54:32 +0900

    Update _card-primary.stories.tsx

--- src/_stories_/components/card/_card-primary.stories.tsx
+++ src/_stories_/components/card/_card-primary.stories.tsx
@@ -24,7 +24,7 @@ export const _CardPrimary: Story = {
   args: {
     icon: <IconCheck />,
     title: 'Card Title',
-    content: 'Card Content',
+    content: '123345667',
     cardType: 'value',
   },
 };
